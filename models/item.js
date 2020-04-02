289b6ba127ddd8a44c7229770c6e5c41bf992e98
Kyle Wilson <kylewilson52@gmail.com>
2020-04-01 23:37:31 -0400

    tables working separately

--- models/item.js
+++ models/item.js
@@ -2,31 +2,16 @@ const Sequelize = require('sequelize');
 const Model = Sequelize.Model;
 const sequelize = require('./index');
 
-class Order extends Model {}
+class Item extends Model {
+}
 
-Order.init({
-    firstName: Sequelize.STRING,
-    lastName: Sequelize.STRING,
-    emailAddress: Sequelize.STRING,
-    address: Sequelize.STRING,
-    phoneNumber: Sequelize.STRING,
-    zipCode: Sequelize.STRING,
-    additionalInfo: Sequelize.STRING,
-    householdNumber: Sequelize.INTEGER,
-    latitude: Sequelize.STRING,
-    longitude: Sequelize.STRING,
-    status: {
-        type: Sequelize.ENUM,
-        values: ['PENDING',
-            'IN PROGRESS',
-            'COMPLETED',
-            'CANCELLED',
-            'ERROR/ACTION']
-    },
-    assignedToDriver: Sequelize.STRING,
-    assignedToOrg: Sequelize.STRING}, {
+Item.init({
+    name: Sequelize.STRING,
+    quantity: Sequelize.STRING
+}, {
     sequelize,
-    modelName: 'order'});
+    modelName: 'item'
+});
 
 
-module.exports = Order;
+module.exports = Item;
--- services/orderService.js
+++ services/orderService.js
@@ -1,5 +1,5 @@
 var Order  = require('../models/Order');
-
+var Item = require('../models/Item')
 async function getOrder(orderId) {
 
     let allOrders = Order.findAll();
@@ -9,7 +9,10 @@ async function getOrder(orderId) {
 async function createOrder() {
 
     let order = Order.create({firstName : 'Kyle', lastName: 'Wilson', 'emailAddress': 'kylewilson52@gmail.com' });
+    let item = Item.create({name: "dumbOrder", quantity: 2});
     console.log(JSON.stringify(order));
+    console.log(JSON.stringify(item));
+
     return "Susess";
 }
 
